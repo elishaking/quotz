d44c6eb3edb46f981452213b4fe80ae9ab5d1bba
King Elisha <ek.chibueze@gmail.com>
2019-05-30 17:38:33 +0100

    app layout

--- lib/main.dart
+++ lib/main.dart
@@ -21,6 +21,7 @@ class MyApp extends StatelessWidget {
         // Notice that the counter didn't reset back to zero; the application
         // is not restarted.
         primarySwatch: Colors.blue,
+        textTheme: TextTheme(body1: TextStyle(color: Colors.white54))
       ),
       home: HomePage(),
     );
--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -8,8 +8,60 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return Scaffold(
+      backgroundColor: Colors.white12,
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+      ),
+      body: Container(
+        padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+        child: Column(
+          children: <Widget>[
+            Expanded(
+              flex: 5,
+              child: Container(
+                padding: EdgeInsets.only(top: 30),
+                child: _buildTime(),
+              ),
+            ),
+            Expanded(
+              flex: 1,
+              child: Text("\"lodxjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjj\"", style: TextStyle(
+                fontSize: 40
+              ),),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  Row _buildTime() {
+    double space = 5;
+    double textSize = 130;
+    return Row(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Text("9", style: TextStyle(
+          fontSize: textSize
+        )),
+        SizedBox(width: space,),
+        Text(":", style: TextStyle(
+          fontSize: textSize
+        )),
+        SizedBox(width: space,),
+        Text("30", style: TextStyle(
+          fontSize: textSize
+        )),
+        SizedBox(width: space,),
+        Padding(
+          padding: const EdgeInsets.only(top: 90),
+          child: Text("PM", style: TextStyle(
+            fontSize: 30
+          )),
+        )
+      ],
     );
   }
 }
