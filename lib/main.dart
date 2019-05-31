1b9cf9a5b3124084629d756e25ce3114cfbe636a
King Elisha <ek.chibueze@gmail.com>
2019-05-31 13:04:38 +0100

    preset

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Quotz',
+      // color: Colors.black,
       theme: ThemeData(
         // This is the theme of your application.
         //
