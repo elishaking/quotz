cb97f3bf58a46f33950d21b94372a64a67ae9129
King Elisha <ek.chibueze@gmail.com>
2019-08-06 22:03:46 +0100

    update

--- lib/services/quotes.dart
+++ lib/services/quotes.dart
@@ -4,8 +4,8 @@ import 'package:quotz/models/settings.dart';
 
 
 class QuotesService{
-  // String _url = "https://quotz.skyblazar.com/api/quote";
-  String _url = "http://192.168.43.130:8000/api/quote";
+  String _url = "https://quotz.skyblazar.com/api/quote";
+  // String _url = "http://192.168.43.130:8000/api/quote";
   String _accessKey = "dU7n@#s3ls/'sj8ksjdmV%42wx'ldjvs&8*AjskU";
   
   Future<String> getQuote(Settings settings) async{
@@ -13,6 +13,7 @@ class QuotesService{
     headers: {
       "Authorization": "Bearer: $_accessKey"
     });
+    print(response.body);
 
     return jsonDecode(response.body)["data"]["quote"];
   }
--- lib/services/settings.dart
+++ lib/services/settings.dart
@@ -7,14 +7,14 @@ import 'package:quotz/models/settings.dart';
 class SettingsService{
   Future<Settings> getSettings() async{
     SharedPreferences pref = await SharedPreferences.getInstance();
-    // pref.clear();
+    // pref.   clear();
     String settings = pref.getString("settings");
     if(settings != null){
-      print("hello");
+      // print("hello");
       Map<String, dynamic> settingsData = jsonDecode(settings);
       return Settings.fromMap(settingsData);
     } else{
-      print("not");
+      // print("not");
       return Settings(
         refreshPeriod: RefreshPeriod.hours,
         refreshPeriodValue: 2,
